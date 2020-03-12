25a9755d45ff733d39c023a5f3a4145b2e7379ef
AfifRudinAfandi <afifceo@gmail.com>
2020-03-12 09:20:47 +0700

    a

--- static/js/app.js
+++ static/js/app.js
@@ -17,7 +17,23 @@
         shrinkItem: true,
         mySlides: 1
       });
-		
+      $('.wrapper-img-link').bxSlider({
+        auto: false,
+        autoControls: true,
+        speed: 500,
+        slideSelector: 'div.img-link',
+        minSlides: 1,
+        maxSlides: 1,
+        moveSlides: 1,
+        pager: false,
+        responsive: true,
+        touchEnabled: true,
+        infiniteLoop: true,
+        slideMargin: 20,
+        shrinkItem: true,
+        slideWidth: 200,
+        mySlides: 1
+      });
 	} else {
     $('.rw-slider').bxSlider({
       auto: false,
@@ -36,22 +52,24 @@
       slideWidth: 350,
       mySlides: 1
     });
+
+    $('.wrapper-img-link').bxSlider({
+      auto: false,
+      autoControls: true,
+      speed: 500,
+      slideSelector: 'div.img-link',
+      minSlides: 5,
+      maxSlides: 5,
+      moveSlides: 1,
+      pager: false,
+      responsive: true,
+      touchEnabled: true,
+      infiniteLoop: true,
+      slideMargin: 20,
+      shrinkItem: true,
+      slideWidth: 200,
+      mySlides: 1
+    });
   }
 
-  $('.wrapper-img-link').bxSlider({
-    auto: false,
-    autoControls: true,
-    speed: 500,
-    slideSelector: 'div.img-link',
-    minSlides: 5,
-    maxSlides: 5,
-    moveSlides: 1,
-    pager: false,
-    responsive: true,
-    touchEnabled: true,
-    infiniteLoop: true,
-    slideMargin: 20,
-    shrinkItem: true,
-    slideWidth: 200,
-    mySlides: 1
-  });
+  
